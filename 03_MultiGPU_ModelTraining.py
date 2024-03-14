5ae35238d284da656a73b020cc24e2c1949b8487
bryansmith-db <bryansmith-db>
2024-03-14 16:22:00 +0000

    Updated notebooks with copyright and package licensing info

--- 03_MultiGPU_ModelTraining.py
+++ 03_MultiGPU_ModelTraining.py
@@ -395,4 +395,13 @@ client.set_registered_model_alias(f"{CATALOG}.{SCHEMA}.{model_name}","Production
 
 # COMMAND ----------
 
-
+# MAGIC %md
+# MAGIC
+# MAGIC &copy; 2024 Databricks, Inc. All rights reserved. The source in this notebook is provided subject to the [Databricks License](https://databricks.com/db-license-source).  All included or referenced third party libraries are subject to the licenses set forth below.
+# MAGIC
+# MAGIC | library                                | description             | license    | source                                              |
+# MAGIC |----------------------------------------|-------------------------|------------|-----------------------------------------------------|
+# MAGIC | pytorch-lightning  | lightweight PyTorch wrapper for ML researchers | Apache Software License (Apache-2.0)   | https://pypi.org/project/pytorch-lightning/   |
+# MAGIC | opencv-python  | Wrapper package for OpenCV python bindings| Apache Software License (Apache 2.0)   | https://pypi.org/project/opencv-python/   |
+# MAGIC | segmentation-models-pytorch  | Image segmentation models with pre-trained backbones. PyTorch. | MIT License (MIT)    | https://pypi.org/project/segmentation-models-pytorch/  |
+# MAGIC | shapely  |Manipulation and analysis of geometric objects |BSD License (BSD 3-Clause)    | https://pypi.org/project/shapely/   |
